b38ace7ed70aa8d2b8d32cadecbfa85aa9aaf177
kimdj4e <122508669+kimdj4e@users.noreply.github.com>
2024-06-07 17:48:29 +0900

    fe

---
+++ A04/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Header />
+    <router-view></router-view>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
